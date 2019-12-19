d2179adf368d4cfdaaf7ae6322041cbedac6f94c
coldice1915 <coldice.zs44@gmail.com>
2019-12-19 14:37:30 -0800

    Add 'delete' function to items

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://jameshan@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
@@ -13,24 +14,33 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False, default=False)
+    completed = db.Column(db.Boolean, nullable=False)
 
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}>'
+        return f'<Todo {self.id} {self.description} {self.completed}>'
 
-@app.route('/')
-def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        Todo.query.filter_by(id=todo_id).delete()
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+    return jsonify({ 'success': True })
 
-@app.route('/todos/create', methods=['POST'])
+@app.route('/todos', methods=['POST'])
 def create_todo():
     error = False
     body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description=description)
+        todo = Todo(description=description, completed=False)
         db.session.add(todo)
         db.session.commit()
+        body['id'] = todo.id
+        body['completed'] = todo.completed
         body['description'] = todo.description
     except:
         error = True
@@ -47,6 +57,7 @@ def create_todo():
 def set_completed_todo(todo_id):
     try:
         completed = request.get_json()['completed']
+        print('completed', completed)
         todo = Todo.query.get(todo_id)
         todo.completed = completed
         db.session.commit()
@@ -54,7 +65,11 @@ def set_completed_todo(todo_id):
         db.session.rollback()
     finally:
         db.session.close()
-        return redirect(url_for('index'))
+    return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.order_by('id').all())
 
 if __name__ == "__main__":
     app.run(debug=True)

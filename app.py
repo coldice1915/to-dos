37ed09bc15dfc88da95529713ba76e5a38234e4b
coldice1915 <coldice.zs44@gmail.com>
2019-12-18 19:36:48 -0800

    Add update functionality (checkbox)

--- app.py
+++ app.py
@@ -18,13 +18,17 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.order_by('id').all())
+
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     error = False
     body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description2=description)
+        todo = Todo(description=description)
         db.session.add(todo)
         db.session.commit()
         body['description'] = todo.description
@@ -39,9 +43,18 @@ def create_todo():
     else:
         return jsonify(body)
 
-@app.route('/')
-def index():
-    return render_template('index.html', data=Todo.query.all())
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+        return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
